741228f52cb4c57bb7e68005433984e1cc79a167
ErickBezerrar <erickbrtrindade@gmail.com>
2022-10-17 20:04:46 -0300

    aritmetrica

---
+++ aritmetrica.py
@@ -0,0 +1,10 @@
+import math
+
+numero = int(input("Digite um número: "))
+x2 = numero * 2
+x3 = numero * 3
+raiz = math.sqrt(numero)
+
+print("O dobro de {} é {}".format(numero, x2))
+print("O triplo de {} é {}".format(numero, x3))
+print("A raiz de {} é {}".format(numero, raiz))
