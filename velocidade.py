d9b68dc5dec16d2edebbf5874ecda98a81352694
ErickBezerrar <erickbrtrindade@gmail.com>
2022-10-17 19:48:32 -0300

    velocidade

--- velocidade.py
+++ velocidade.py
@@ -4,7 +4,7 @@ if usuario <= 80:
     print("Sua velocidade é de 80 km/h, não houve multa")
 elif usuario == 90:
     print("Sua velocidade, é de 90 km/h, levou multa leve")
-elif usuario >= (91,100):
+elif usuario >= 91 or 92 or 93 or 91 or 95 or 96 or 97 or 98 or 99 or 100:
     print("Sua velocidade é de {usuario} km/h, levou multra grave")
 elif usuario > 100:
     print("Sua velocidade é de {usuario} km/h, levou multa gravíssima")
