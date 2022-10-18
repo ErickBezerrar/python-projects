04ea96f70c73a27c903b0506ed54d37c905a9fdb
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-10-18 15:59:38 -0300

    fix

---
+++ desconto.py
@@ -0,0 +1,3 @@
+preco = float(input("Informe o valor do produto para dar o desconto: "))
+desconto = preco * 0.95
+print("O produto que custava {} com o desconto de 5% custa {} R$".format(preco,desconto))
