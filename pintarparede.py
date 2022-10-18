0b3c0633a5622f68818f01410a7915c87119081f
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-10-18 15:49:53 -0300

    pintar

--- pintarparede.py
+++ pintarparede.py
@@ -1,1 +1,7 @@
-altura = int(input())
+altura = float(input("Insira a altura da parede em metros: "))
+largura = float(input("Insira a largura da parede em metros: "))
+
+total_parede = altura * largura
+print("Sua parede tem dimensão de {}x{} e sua área é de {}m2".format(altura, largura, total_parede))
+tinta = total_parede / 2
+print("Para pintar essa parede você precisará de {} litros".format(tinta))
