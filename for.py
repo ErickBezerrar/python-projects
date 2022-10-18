1306e95ff4102534e66cc3d564a08015ae7a66f2
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-10-18 17:56:17 -0300

    turtle

--- for.py
+++ for.py
@@ -1,9 +1,21 @@
+import turtle
+tartaruga = turtle.Turtle()
+tartaruga.shape("turtle")
+tartaruga.color("medium aquamarine")
+
 for palavra in range(1,4):
     print("Carregando...")
 
+x = 5
 
 for lista in range(1,20):
     print(lista)
+    
 print("===========")
 for lista_dois in range(1,20,2):
     print(lista_dois)
+    
+print("===========")
+for lista_tres in range(1,101,x):
+    print(lista_tres)
+
