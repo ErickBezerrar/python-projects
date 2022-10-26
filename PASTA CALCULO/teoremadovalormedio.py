fb164563b12bedcd3dc7289f7c13c5db2dc2a16b
ErickBezerrar <erickbrtrindade@gmail.com>
2022-10-25 23:43:38 -0300

    grafico

--- PASTA CALCULO/teoremadovalormedio.py
+++ PASTA CALCULO/teoremadovalormedio.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib
+import matplotlib.pyplot as plt
 
 def calculafuncao(funcao, t):
     funcao = funcao.replace("x", str(t))
@@ -21,8 +22,8 @@ def testatmv(funcao, lista, a,b,h):
         if round(funcaoderivada(funcao,i,h), 2) == funcaotaxa(funcao, a,b):
             print(i)
 
-a = float(input("informe um valor: "))
-b = float(input("informe um valor: "))
+a = float(input("informe um valor para a: "))
+b = float(input("informe um valor para b: "))
 funcao = input("Digite sua funcão: ")
 lista = []
 h = 0.0001
@@ -31,3 +32,8 @@ for i in np.arange(a,b,0.001):
     lista.append(i)
     
 print(testatmv(funcao, lista,a,b,h))
+
+plt.ylabel("Y:Variação")
+plt.xlabel("X:Pontos A e B")
+plt.plot(a,b)
+plt.show()
