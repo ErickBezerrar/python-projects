819e6ccce04e14eb1ed635b5f56fe6f4268a3b0d
ErickBezerrar <erickbrtrindade@gmail.com>
2023-05-06 15:21:59 -0300

    python projeto ODONTO

--- ODONTO/binarizacao.py
+++ ODONTO/binarizacao.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-df = pd.read_excel('xlsx')
-colunas = ['colunas que deverão ser binarizadas(PELO NOME)']
+# leia o arquivo excel
+df = pd.read_excel('')
 
-df[colunas] = df[colunas].apply(lambda x: pd.Series(pd.factorize(x)[0])).astype('category')
+# visualize o conteúdo do dataframe
+print(df.head())
 
-df.to_excel('caminho_do_novo_arquivo.xlsx', index=False)
+#colunas = ['colunas que deverão ser binarizadas(PELO NOME)']
+#df[colunas] = df[colunas].apply(lambda x: pd.Series(pd.factorize(x)[0])).astype('category')
+#df.to_excel('caminho_do_novo_arquivo.xlsx', index=False)
