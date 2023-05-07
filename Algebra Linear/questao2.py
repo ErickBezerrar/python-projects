a376fae014c499c52b078c26c6c6b7c21c581799
ErickBezerrar <erickbrtrindade@gmail.com>
2023-05-06 21:49:55 -0300

    Equação polinomial

--- Algebra Linear/questao2.py
+++ Algebra Linear/questao2.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-velocity = [0, 2, 4, 6, 8, 10]
-force = [0, 2.90, 14.8, 39.6, 74.3, 119]
+velocidade = [0, 2, 4, 6, 8, 10]
+forca = [0, 2.90, 14.8, 39.6, 74.3, 119]
 
-coefficients = np.polyfit(velocity, force, 5)
-polynomial = np.poly1d(coefficients)
+coeficientes = np.polyfit(velocidade, forca, 5)
+polinomial = np.poly1d(coeficientes)
 
-velocity_estimate = 750
-force_estimate = polynomial(velocity_estimate)
+velocidade_estimada = 750
+forca_estimada = polinomial(velocidade_estimada)
 
-plt.scatter(velocity, force, color='red', label='Dados')
-plt.plot(velocity, polynomial(velocity), label='Polinômio Interpolador')
+plt.scatter(velocidade, forca, color='red', label='Dados')
+plt.plot(velocidade, polinomial(velocidade), label='Polinômio Interpolador')
 plt.xlabel('Velocidade (100 ft/sec)')
 plt.ylabel('Força (100 lb)')
 plt.title('Dados e Polinômio Interpolador')
@@ -19,5 +19,5 @@ plt.legend()
 plt.show()
 
 print("O polinômio interpolador é: ")
-print(f"p(t) = {coefficients[5]:.4f} + {coefficients[4]:.4f}t + {coefficients[3]:.4f}t^2 + {coefficients[2]:.4f}t^3 + {coefficients[1]:.4f}t^4 + {coefficients[0]:.4f}t^5")
-print(f"A força estimada na velocidade de {velocity_estimate} ft/sec é de {force_estimate:.4f} lb.")
+print(f"p(t) = {coeficientes[5]:.4f} + {coeficientes[4]:.4f}t + {coeficientes[3]:.4f}t^2 + {coeficientes[2]:.4f}t^3 + {coeficientes[1]:.4f}t^4 + {coeficientes[0]:.4f}t^5")
+print(f"A força estimada na velocidade de {velocidade_estimada} ft/sec é de {forca_estimada:.4f} lb.")
