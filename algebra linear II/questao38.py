7e04db91f0d055c06efa9be8a4413da93d8b2434
ErickBezerrar <erickbrtrindade@gmail.com>
2023-05-23 09:05:06 -0300

    questao 38

---
+++ algebra linear II/questao38.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+np.random.seed(0)  # Setting a seed for reproducibility
+
+for i in range(3, 6):
+    J = np.random.randint(1, 11, size=(5, i))
+    K = np.random.randint(1, 11, size=(i, 7))
+
+    A = np.matmul(J, K)
+    rref_A = np.linalg.matrix_rank(A)
+
+    print("A:\n", A)
+    print("Rank of A:", rref_A)
+
+    esc = np.linalg.matrix_rank(A)
+    C = A[:, 0:i]
+    R = esc[0:i, :]
+
+    CR = np.matmul(C, R)
+    print("CR:\n", CR)
+    print("-------------------")
