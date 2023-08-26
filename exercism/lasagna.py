a8abe5eae3b0b341a7ccec720fea9518cf76c843
ErickBezerrar <erickbrtrindade@gmail.com>
2023-08-26 10:17:07 -0300

    updates

--- exercism/lasagna.py
+++ exercism/lasagna.py
@@ -9,16 +9,16 @@ def preparation_time_in_minutes(camadas):
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     return number_of_layers * 2 + elapsed_bake_time
     
-    """Calculate the elapsed cooking time.
+    #"""Calculate the elapsed cooking time.
 
-    :param number_of_layers: int - the number of layers in the lasagna.
-    :param elapsed_bake_time: int - elapsed cooking time.
-    :return: int - total time elapsed (in minutes) preparing and cooking.
+    #   :param number_of_layers: int - the number of layers in the lasagna.
+    #:param elapsed_bake_time: int - elapsed cooking time.
+    #:return: int - total time elapsed (in minutes) preparing and cooking.
 
-    This function takes two integers representing the number of lasagna layers and the
-    time already spent baking and calculates the total elapsed minutes spent cooking the
-    lasagna.
-    """
+    #This function takes two integers representing the number of lasagna layers and the
+    #time already spent baking and calculates the total elapsed minutes spent cooking the
+    #lasagna.
+    
 bake_time_remaining(30)
 preparation_time_in_minutes(2)
 elapsed_time_in_minutes(3, 20)
